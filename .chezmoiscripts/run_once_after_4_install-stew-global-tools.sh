3128505b88707d4722079ecb40bcde4562785daf
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2025-07-03 03:50:32 +0100

    adds a clear to stew install script

--- .chezmoiscripts/run_once_after_4_install-stew-global-tools.sh
+++ .chezmoiscripts/run_once_after_4_install-stew-global-tools.sh
@@ -8,5 +8,6 @@ echo ""
 if [ -x "$HOME/.local/share/mise/shims/stew" ]; then
   echo "--- Installing all stew global tools ---"
   echo "N\n" | "$HOME/.local/share/mise/shims/stew" i "$HOME/.config/stew/Stewfile" 2>/dev/null
+  clear
   echo "--- Finished installing all stew global tools ---"
 fi
