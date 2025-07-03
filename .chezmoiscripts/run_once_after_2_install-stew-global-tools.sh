b7838251aea6a371b7c46b3f00eedfbb61cda777
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2025-07-03 02:37:38 +0100

    updates to stew file

--- .chezmoiscripts/run_once_after_2_install-stew-global-tools.sh
+++ .chezmoiscripts/run_once_after_2_install-stew-global-tools.sh
@@ -7,6 +7,6 @@ echo ""
 
 if [ -x "$HOME/.local/share/mise/shims/stew" ]; then
   echo "--- Installing all stew global tools ---"
-  "$HOME/.local/bin/stew" install "$HOME/.config/Stewfile" 2>/dev/null
+  yes N | "$HOME/.local/share/mise/shims/stew" i "$HOME/.config/stew/Stewfile" 2>/dev/null
   echo "--- Finished installing all stew global tools ---"
 fi
