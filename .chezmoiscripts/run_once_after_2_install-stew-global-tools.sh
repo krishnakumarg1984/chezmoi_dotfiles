df651dd03e4f3056a42a5bba02ceb775eff0cb00
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2025-07-03 02:24:31 +0100

    stewfile for vale-ls

---
+++ .chezmoiscripts/run_once_after_2_install-stew-global-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash --login
+
+# need a login shell for sourcing the shell's startup scripts so that environment variables for tools are set correctly
+set -eu
+
+echo ""
+
+if [ -x "$HOME/.local/share/mise/shims/stew" ]; then
+  echo "--- Installing all stew global tools ---"
+  "$HOME/.local/bin/stew" install "$HOME/.config/Stewfile"
+  echo "--- Finished installing all stew global tools ---"
+fi
