b5b9299a032beeaf3207307793201c62a1ab7898
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2025-07-03 02:25:16 +0100

    ignore stew install errors

--- .chezmoiscripts/run_once_after_2_install-stew-global-tools.sh
+++ .chezmoiscripts/run_once_after_2_install-stew-global-tools.sh
@@ -7,6 +7,6 @@ echo ""
 
 if [ -x "$HOME/.local/share/mise/shims/stew" ]; then
   echo "--- Installing all stew global tools ---"
-  "$HOME/.local/bin/stew" install "$HOME/.config/Stewfile"
+  "$HOME/.local/bin/stew" install "$HOME/.config/Stewfile" 2>/dev/null
   echo "--- Finished installing all stew global tools ---"
 fi
