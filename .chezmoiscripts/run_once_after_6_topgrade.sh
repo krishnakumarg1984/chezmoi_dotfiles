33b8920aa810242cfa092220218899818462592e
Krishnakumar <krishnakumar@vt.edu>
2025-07-04 19:18:35 +0100

    updates to topgrade.toml

--- .chezmoiscripts/run_once_after_6_topgrade.sh
+++ .chezmoiscripts/run_once_after_6_topgrade.sh
@@ -6,7 +6,7 @@ echo ""
 if [ -x "$HOME/.config/pixi/bin/topgrade" ] &>/dev/null; then
   echo "--- Updating all installed tools with topgrade ---"
 
-  "$HOME/.config/pixi/bin/topgrade" --yes --cleanup --disable waydroid --disable system --disable flatpak --disable firmware --disable conda --disable chezmoi
+  "$HOME/.config/pixi/bin/topgrade" || true
 
   echo "--- Finished updating all installed tools with topgrade  --"
 fi
