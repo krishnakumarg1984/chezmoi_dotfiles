847f0874d487c935e1dcc7e1edb1331c8ace4774
Krishnakumar <krishnakumar@vt.edu>
2025-07-04 03:46:11 +0100

    adds topgrade

--- .chezmoiscripts/run_once_2_install-pixi.sh
+++ .chezmoiscripts/run_once_2_install-pixi.sh
@@ -7,6 +7,6 @@ echo "--- Installing Pixi ---"
 if command -v pixi || [ -x "$HOME/.config/pixi/bin/pixi" ] || [ -x "$HOME/.pixi/bin/pixi" ] &>/dev/null; then
   echo "Pixi is already installed"
 else
-  curl -fsSL https://pixi.sh/install.sh | PIXI_HOME="$HOME/.config/pixi" bash
+  curl -fsSL https://pixi.sh/install.sh | PIXI_HOME="$HOME/.config/pixi" PIXI_NO_PATH_UPDATE=1 bash
 fi
 echo "--- Finished installing Pixi ---"
---
+++ .chezmoiscripts/run_once_after_6_topgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash --login
+
+set -eu
+
+echo ""
+if [ -x "$HOME/.config/pixi/bin/topgrade" ] &>/dev/null; then
+  echo "--- Updating all installed tools with topgrade ---"
+
+  "$HOME/.config/pixi/bin/topgrade" --yes --cleanup --disable waydroid --disable chezmoi && clear
+
+  echo "--- Finished updating all installed tools with topgrade  --"
+fi
