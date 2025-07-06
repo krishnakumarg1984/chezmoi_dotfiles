d60f2ce0bffc9f6f00ca3fa59f042a842a6e0a70
Krishnakumar <krishnakumar@vt.edu>
2025-07-06 18:33:15 +0100

    updates to mise config

--- .chezmoiscripts/run_once_after_4_install-stew-global-tools.sh
+++ .chezmoiscripts/run_once_after_4_install-stew-global-tools.sh
@@ -7,6 +7,7 @@ echo ""
 
 if [ -x "$HOME/.local/share/mise/shims/stew" ]; then
   echo "--- Installing all stew global tools ---"
+  \rm -rf "$HOME/.local/share/stew/Stewfile.lock.json"
   echo "N\n" | "$HOME/.local/share/mise/shims/stew" i "$HOME/.config/stew/Stewfile" 2>/dev/null
   clear
   echo "--- Finished installing all stew global tools ---"
