93b5b40777227c9fbc099fb8ed631f7150c9fadc
Krishnakumar <krishnakvtedu@gmail.com>
2025-11-14 00:17:00 +0530

    set mise to be quiet

--- .chezmoiscripts/run_once_after_1_install-mise-global-tools.sh
+++ .chezmoiscripts/run_once_after_1_install-mise-global-tools.sh
@@ -17,6 +17,6 @@ if [ -x "$(command -v mise)" ]; then
   MISE_JOBS=$((NCORES < 8 ? NCORES : 8)) # At most 8 cores. https://stackoverflow.com/a/10415158
 
   # MISE_QUIET=1 MISE_VERBOSE=1
-  "$HOME/.local/bin/mise" --jobs "$MISE_JOBS" install
+  "$HOME/.local/bin/mise" --quiet --jobs "$MISE_JOBS" install
   echo "--- Finished installing all mise global tools ---"
 fi
